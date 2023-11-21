234f682d1d3b778d12c33013c1a734e5ff5b3540
RegeyNicole0 <102006874+RegeyNicole0@users.noreply.github.com>
2023-11-21 20:12:24 +0800

    pipfile

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # CSC173 Project
  CSC173 Collaborative Final Project
+
+ PLEASE USE VIRTUAL ENVIRONMENT!!
+
+FOR YOLO:
+pip install ultralytics
---
+++ run.py
@@ -0,0 +1,29 @@
+import cv2
+import argparse
+
+def parse_arguments() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(description="Censor Cam")
+    parser.add_argument("--webcam-resolution", default=[1280, 720], nargs=2, type=int)
+    args = parser.parse_args()
+    return args
+
+def main():
+    args = parse_arguments()
+    frame_width, frame_height = args.webcam_resolution
+
+    cap = cv2.VideoCapture(0)
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, frame_width)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, frame_height)
+    
+    while True:
+        ret, frame = cap.read()
+        cv2.imshow("censorcam", frame)
+
+        # print(frame.shape)
+        # break
+        
+        if (cv2.waitKey(30) == 27):
+            break
+
+if __name__== "__main__":
+    main()
