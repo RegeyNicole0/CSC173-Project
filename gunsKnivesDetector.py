1cd0f13ca9143f170e714f076ac96026efe41f45
Janella Suzanne Balantac <janellasuzanne.balantac@g.msuiit.edu.ph>
2023-11-24 19:36:39 +0800

    Real-Time Prediction

--- gunsKnivesDetector.py
+++ gunsKnivesDetector.py
@@ -2,5 +2,5 @@ from ultralytics import YOLO
 # from ultralytics.yolo.v8.detect.predict import DetectionPredictor
 import cv2
 
-model = YOLO("detector1\gunsKnivesDetector.pt")
+model = YOLO("gunsKnivesDetector.pt")
 model.predict(source="0", show=True, conf=0.5) # accepts all formats
--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import cv2
 import argparse
+from ultralytics import YOLO
 
 def parse_arguments() -> argparse.Namespace:
     parser = argparse.ArgumentParser(description="Censor Cam")
@@ -14,16 +15,32 @@ def main():
     cap = cv2.VideoCapture(0)
     cap.set(cv2.CAP_PROP_FRAME_WIDTH, frame_width)
     cap.set(cv2.CAP_PROP_FRAME_HEIGHT, frame_height)
-    
+
+    model = YOLO("gunsKnivesDetector.pt")
+
     while True:
         ret, frame = cap.read()
-        cv2.imshow("censorcam", frame)
 
-        # print(frame.shape)
-        # break
-        
+        # Perform YOLO prediction on the frame
+        results = model.predict(frame, conf=0.5)
+
+        # Iterate through the list of results and draw bounding boxes on the frame
+        for result in results:
+            # Accessing bounding box information using the 'boxes' attribute
+            if result.boxes is not None:
+                for box in result.boxes.xyxy:
+                    xmin, ymin, xmax, ymax = box
+                    cv2.rectangle(frame, (int(xmin), int(ymin)), (int(xmax), int(ymax)), (0, 255, 0), 2)
+
+        # Display the frame with bounding boxes
+        cv2.imshow("Object Detection", frame)
+
+        # Check for the 'Esc' key to exit
         if (cv2.waitKey(30) == 27):
             break
+
+    cap.release()
+    cv2.destroyAllWindows()
 
 if __name__== "__main__":
     main()
