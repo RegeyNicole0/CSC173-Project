e84992af2308e046aa12028071dd6211c023e7f4
Janella Suzanne Balantac <janellasuzanne.balantac@g.msuiit.edu.ph>
2023-12-08 22:03:05 +0800

    Feat: Save Live Stream Video

--- README.md
+++ README.md
@@ -26,3 +26,14 @@ To test run this script in command line:
 yolo task=detect mode=predict model=<modelname>.pt show=True source= <source to test>
 
 Note: Make sure to copy the pretrained model, as well as resources to the main folder to test!!
+
+## ---
+
+yolo task=detect mode=predict model=last.pt show=True conf=0.5 source=test_video.mp4
+
+BLURRING
+Video:
+python test.py --input-type video --input-path test_video2.mp4 --output-path best/out_test_video2.mp4
+
+Image:
+python test.py --input-type image --input-path image_test_blur --output-path best
--- run.py
+++ run.py
@@ -1,3 +1,5 @@
+# REAL-TIME DETECTION AND BLURRING
+
 import cv2
 import argparse
 from ultralytics import YOLO
@@ -5,6 +7,7 @@ from ultralytics import YOLO
 def parse_arguments() -> argparse.Namespace:
     parser = argparse.ArgumentParser(description="Censor Cam")
     parser.add_argument("--webcam-resolution", default=[1280, 720], nargs=2, type=int)
+    parser.add_argument("--output-path", default="output.avi", type=str, help="Path to save the output video")
     args = parser.parse_args()
     return args
 
@@ -14,7 +17,7 @@ def blur_boxes(frame, boxes):
         # Extract the region inside the bounding box
         roi = frame[int(ymin):int(ymax), int(xmin):int(xmax)]
         # Apply Gaussian blur to the region
-        blurred_roi = cv2.GaussianBlur(roi, (51, 51), 0)
+        blurred_roi = cv2.GaussianBlur(roi, (135, 135), 0)
         # Replace the original region with the blurred one
         frame[int(ymin):int(ymax), int(xmin):int(xmax)] = blurred_roi
     return frame
@@ -27,7 +30,11 @@ def main():
     cap.set(cv2.CAP_PROP_FRAME_WIDTH, frame_width)
     cap.set(cv2.CAP_PROP_FRAME_HEIGHT, frame_height)
 
-    model = YOLO("gunsKnivesDetector.pt")
+    model = YOLO("model_training/train/weights/best.pt")
+
+    # Define the codec and create a VideoWriter object
+    fourcc = cv2.VideoWriter_fourcc('M','J','P','G')
+    out = cv2.VideoWriter(args.output_path, fourcc, 20.0, (frame_width, frame_height), isColor=True)
 
     while True:
         ret, frame = cap.read()
@@ -42,6 +49,9 @@ def main():
                 # Call the blur_boxes function to apply Gaussian blur to the regions inside the bounding boxes
                 frame = blur_boxes(frame, result.boxes.xyxy)
 
+        # Write the frame to the output video file
+        out.write(frame)
+
         # Display the frame with bounding boxes and blurred regions
         cv2.imshow("Object Detector", frame)
 
@@ -50,6 +60,7 @@ def main():
             break
 
     cap.release()
+    out.release()
     cv2.destroyAllWindows()
 
 if __name__ == "__main__":
